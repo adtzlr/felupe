e81ec1b9510d928ee8f7ada283f1ed9366e97313
Andreas D <a.dutzler@gmail.com>
2021-04-23 15:03:06 +0200

    Update script_101.py

--- scripts/script_101.py
+++ scripts/script_101.py
@@ -7,16 +7,8 @@ Created on Tue Apr 20 08:45:35 2021
 
 import numpy as np
 
-from mesh import Cube
-from element import Hex1
-from domain import Domain
-import quadrature
-import doftools
-from doftools import Boundary
-import solve
-import constitution
-
-from helpers import identity, dya, det, cof, transpose, dot, eigvals
+import felupe as fe
+from felupe.helpers import identity, dya, det, cof, transpose, dot, eigvals
 
 tol = 1e-5
 
@@ -26,13 +18,13 @@ mu = 1.0
 bulk = 5000.0 * mu
 cargs = [mu, bulk]
 
-e = Hex1()
-m = Cube(a=(0, 0, 0), b=(2, 2, 1), n=(10, 10, 5))
+e = fe.element.Hex1()
+m = fe.mesh.Cube(a=(0, 0, 0), b=(2, 2, 1), n=(10, 10, 5))
 # m.nodes[:,:2] = 0.8*m.nodes[:,:2] + 0.2*m.nodes[:,:2] * (m.nodes[:,2]**7).reshape(-1,1)
-q = quadrature.Linear(dim=3)
-c = constitution.NeoHooke()
+q = fe.quadrature.Linear(dim=3)
+c = fe.constitution.NeoHooke()
 
-d = Domain(e, m, q)
+d = fe.Domain(e, m, q)
 
 # undeformed element volumes
 V = d.volume()
@@ -46,10 +38,10 @@ J = np.ones(d.nelements)
 f0 = lambda x: np.isclose(x, 0)
 f1 = lambda x: np.isclose(x, 1)
 
-symx = Boundary(d.dof, m, "sym-x", skip=(0, 1, 1), fx=f0)
-symy = Boundary(d.dof, m, "sym-y", skip=(1, 0, 1), fy=f0)
-symz = Boundary(d.dof, m, "sym-z", skip=(1, 1, 0), fz=f0)
-movz = Boundary(d.dof, m, "move", skip=(1, 1, 0), fz=f1, value=move)
+symx = fe.Boundary(d.dof, m, "sym-x", skip=(0, 1, 1), fx=f0)
+symy = fe.Boundary(d.dof, m, "sym-y", skip=(1, 0, 1), fy=f0)
+symz = fe.Boundary(d.dof, m, "sym-z", skip=(1, 1, 0), fz=f0)
+movz = fe.Boundary(d.dof, m, "move", skip=(1, 1, 0), fz=f1, value=move)
 bounds = [symx, symy, symz, movz]
 
 # symx = Boundary(d.dof, m, "sym-x", skip=(0, 1, 1), fx=f0)
@@ -60,10 +52,10 @@ bounds = [symx, symy, symz, movz]
 # bounds = [symx, symy, fixb, fixt, movt]
 
 # dofs to "D"ismiss and to "I"ntegrate
-D, I = doftools.partition(d.dof, bounds)
+D, I = fe.doftools.partition(d.dof, bounds)
 
 # obtain external displacements for prescribed dofs
-uDext = doftools.apply(u, d.dof, bounds, D)
+uDext = fe.doftools.apply(u, d.dof, bounds, D)
 
 n_ = []
 
@@ -89,8 +81,8 @@ for iteration in range(100):
     r = r1 + r2 + r3
     K = d.asmatrix(d.integrate(c.A(F, p, J, *cargs)) + d2UdJ2 * V * dya(H, H))
 
-    system = solve.partition(u, r, K, I, D)
-    du = solve.solve(*system, uDext)
+    system = fe.solve.partition(u, r, K, I, D)
+    du = fe.solve.solve(*system, uDext)
     dJ = np.einsum("aie,eai->e", H, du[m.connectivity])
     dp = dJ * d2UdJ2
 

2881541ef76f2686c4cf626dda120199503871ca
Andreas D <a.dutzler@gmail.com>
2021-04-24 23:18:43 +0200

    Update solve.py
    rename dof sets to dof0 and dof1

--- felupe/solve.py
+++ felupe/solve.py
@@ -25,27 +25,26 @@ along with Felupe.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
-
 import numpy as np
 
-from scipy.sparse.linalg import spsolve
-
-# from pypardiso import spsolve
-
-
-def partition(u, r, K, I, D):
-    # uI = u.ravel()[I]
-    uD = u.ravel()[D]
-    rI = r[I]
-    KII = K[I, :][:, I]
-    KID = K[I, :][:, D]
-    return u, rI, uD, KII, KID, I, D
+try:
+    from pypardiso import spsolve
+except:
+    from scipy.sparse.linalg import spsolve
 
 
-def solve(u, rI, uD, KII, KID, I, D, uDext):
-    drD = KID.dot(uDext - uD)
-    duI = spsolve(KII, -rI - drD.reshape(*rI.shape))
-    du = np.zeros(u.size)
-    du[I] = duI
-    du[D] = uDext - uD
+def partition(u, r, K, dof1, dof0):
+    u0 = u.ravel()[dof0]
+    r1 = r[dof1]
+    K11 = K[dof1, :][:, dof1]
+    K10 = K[dof1, :][:, dof0]
+    return u, r1, u0, K11, K10, dof1, dof0
+
+
+def solve(u, r1, u0, K11, K10, dof1, dof0, u0ext):
+    dr0 = K10.dot(u0ext - u0)
+    du1 = spsolve(K11, -r1 - dr0.reshape(*r1.shape))
+    du = np.empty(u.size)
+    du[dof1] = du1
+    du[dof0] = u0ext - u0
     return du.reshape(*u.shape)

440fafba309c25be22d50aabfb650ed61a720a5b
Andreas D <a.dutzler@gmail.com>
2021-05-06 16:59:50 +0200

    Update quadrature.py
    add Quadratic and Cubic schemes using quadpy

--- felupe/quadrature.py
+++ felupe/quadrature.py
@@ -27,6 +27,8 @@ along with Felupe.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
 
+import quadpy
+
 class Constant:
     
     def __init__(self, dim=3):
@@ -83,7 +85,7 @@ class _P1_D3:
     
     def __init__(self):
         # integration point weights and coordinates
-        self.npoints = 8
+        self.npoints = 2**3
         self.weights = np.ones(8)
         self.points = np.array([[-1,-1,-1],
                                 [ 1,-1,-1],
@@ -98,7 +100,7 @@ class _P1_D2:
     
     def __init__(self):
         # integration point weights and coordinates
-        self.npoints = 4
+        self.npoints = 2**2
         self.weights = np.ones(4)
         self.points = np.array([[-1,-1],
                                 [ 1,-1],
@@ -113,3 +115,98 @@ class _P1_D1:
         self.weights = np.ones(2)
         self.points = np.array([[-1],
                                 [ 1]]) * np.sqrt(1/3)
+        
+        
+class Quadratic:
+    
+    def __init__(self, dim=3):
+        # integration point weights and coordinates
+        if dim == 3:
+            self.scheme = _P2_D3()
+        elif dim == 2:
+            self.scheme = _P2_D2()
+        elif dim == 1:
+            self.scheme = _P2_D1()
+        self.npoints = self.scheme.npoints
+        self.weights = self.scheme.weights
+        self.points = self.scheme.points
+    
+class _P2_D3:
+    
+    def __init__(self):
+        # integration point weights and coordinates
+        self.npoints = 3**3
+
+        scheme = quadpy.c3.product(quadpy.c1.gauss_legendre(3))
+        
+        self.points = scheme.points.T
+        self.weights = scheme.weights
+
+class _P2_D2:
+    
+    def __init__(self):
+        # integration point weights and coordinates
+        self.npoints = 3**2
+        
+        scheme = quadpy.c2.product(quadpy.c1.gauss_legendre(3))
+        
+        self.points = scheme.points.T
+        self.weights = scheme.weights
+
+class _P2_D1:
+    
+    def __init__(self):
+        # integration point weights and coordinates
+        self.npoints = 3
+
+        scheme = quadpy.c1.gauss_legendre(3)
+        
+        self.points = scheme.points.T
+        self.weights = scheme.weights
+        
+class Cubic:
+    
+    def __init__(self, dim=3):
+        # integration point weights and coordinates
+        if dim == 3:
+            self.scheme = _P3_D3()
+        elif dim == 2:
+            self.scheme = _P3_D2()
+        elif dim == 1:
+            self.scheme = _P3_D1()
+        self.npoints = self.scheme.npoints
+        self.weights = self.scheme.weights
+        self.points = self.scheme.points
+    
+class _P3_D3:
+    
+    def __init__(self):
+        # integration point weights and coordinates
+        self.npoints = 4**3
+        
+        scheme = quadpy.c3.product(quadpy.c1.gauss_legendre(4))
+        
+        self.points = scheme.points.T
+        self.weights = scheme.weights
+
+class _P3_D2:
+    
+    def __init__(self):
+        # integration point weights and coordinates
+        self.npoints = 4**2
+        
+        scheme = quadpy.c2.product(quadpy.c1.gauss_legendre(4))
+        
+        self.points = scheme.points.T
+        self.weights = scheme.weights
+
+class _P3_D1:
+    
+    def __init__(self):
+        # integration point weights and coordinates
+        self.npoints = 4
+        
+        scheme = quadpy.c1.gauss_legendre(4)
+        
+        self.points = scheme.points.T
+        self.weights = scheme.weights

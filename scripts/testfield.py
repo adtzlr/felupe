afa9c9c4dd5867ad54e430f01386a31e3d47a479
Andreas D <a.dutzler@gmail.com>
2021-05-12 14:28:56 +0200

    Update testfield.py

--- scripts/testfield.py
+++ scripts/testfield.py
@@ -8,10 +8,10 @@ Created on Fri May  7 13:41:18 2021
 import numpy as np
 import felupe as fe
 
-m = fe.mesh.Cube(n=21)
+m = fe.mesh.Cube(n=6)
 q = fe.quadrature.Linear(3)
 
-m0 = fe.mesh.convert(m)
+m0 = fe.mesh.convert(m, order=0)
 
 e1 = fe.element.Hex1()
 r1 = fe.region.Region(m, e1, q)
@@ -26,22 +26,13 @@ pf = fe.Field(r0, 1, values=-1)
 Jf = fe.Field(r0, 1, values=1)
 fields = (uf, pf, Jf)
 
-
-def convert(fields):
-    u, p, J = fields
-    return (u.grad() + fe.math.identity(u.grad()), p.interpolate(), J.interpolate())
-
-
-F, p, J = convert(fields)
-
+F, p, J = fe.field.extract(fields, add_identity=True)
 
 def f(F, p, J):
     return p * fe.math.cof(F)
 
-
 def g(F, p, J):
     return p * fe.math.dya(F, F)
-
 
 r = fe.IntegralForm(f(F, p, J), uf, dV, grad_v=True).assemble()
 K = fe.IntegralForm(g(F, p, J), uf, dV, uf, grad_v=True, grad_u=True).assemble()

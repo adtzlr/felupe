de1efcdc73faba194e0103ae3e294e3062e720c9
Andreas D <a.dutzler@gmail.com>
2021-05-27 09:27:23 +0200

    remove unused features

--- felupe/mesh.py
+++ felupe/mesh.py
@@ -115,25 +115,6 @@ class Line(Mesh):
         self.edgenodes = 2
 
 
-class CubeQuadratic(Mesh):
-    def __init__(self, a=(0, 0, 0), b=(1, 1, 1)):
-        self.a = a
-        self.b = b
-        self.n = (3, 3, 3)
-
-        nodes, connectivity = cube_hexa(a, b, n=self.n)
-        etype = "hexahedron20"
-
-        super().__init__(nodes, connectivity, etype)
-        self.edgenodes = 8
-
-        self.nodes = self.nodes[
-            [0, 2, 8, 6, 18, 20, 26, 24, 1, 5, 7, 3, 19, 23, 25, 21, 9, 11, 17, 15]
-        ]
-        self.connectivity = np.arange(20).reshape(1, -1)
-        self.update(self.connectivity)
-
-
 class CubeAdvanced(Cube):
     def __init__(
         self,
@@ -168,68 +149,6 @@ class CubeAdvanced(Cube):
         z[:, 2] *= H / 2
         self.nodes = z
         self.update(self.connectivity)
-
-
-class CylinderAdvanced(Mesh):
-    def __init__(
-        self,
-        D=10,
-        H=1,
-        n=(13, 13, 9),
-        d=2,
-        phi=180,
-        dD=1,
-        dd=1,
-        k=4,
-        align=True,
-        symmetry=False,
-    ):
-
-        R = D / 2
-        r = d / 2
-        # rm = (R + r) / 2
-        dr = R - r
-
-        if symmetry:
-            a = 0
-        else:
-            a = -1
-
-        N, C = expand(line_line(a=a, b=1, n=n[2]), n[0], z=2)
-        N[:, 1] -= 1
-
-        bottom = N[:, 1] < 0
-        top = N[:, 1] > 0
-        Nb = N[bottom]
-        Nt = N[top]
-
-        Nb[:, 1] *= 1 + dd / dr * Nb[:, 0] ** k
-        Nt[:, 1] *= 1 + dD / dr * Nt[:, 0] ** k
-
-        N[bottom] = Nb
-        N[top] = Nt
-
-        N[:, 1] += 1
-        N[:, 1] *= dr / 2
-        N[:, 1] += r
-
-        N[:, 0] *= H / 2
-        N[:, 0] += H / 2
-
-        nodes, connectivity = revolve((N, C), n[1], -phi, axis=0)
-        etype = "hexahedron"
-
-        if align:
-            nodes, connectivity = rotate(rotate((nodes, connectivity), 90, 1), 90, 2)
-
-        super().__init__(nodes, connectivity, etype)
-        self.edgenodes = 8
-
-
-class Cylinder(CylinderAdvanced):
-    def __init__(self, D=2, H=1, n=(3, 9, 3), phi=360):
-
-        super().__init__(D, H, n, d=0, phi=phi, dD=0, dd=0, k=4, align=True)
 
 
 # line, rectangle (based on line) and cube (based on rectangle) generators
@@ -305,88 +224,6 @@ def expand(mesh, n=11, z=1):
 
     c = [Connectivity + len(p) * a for a in np.arange(n)]
     connectivity = np.vstack([np.hstack((a, b[:, sl])) for a, b in zip(c[:-1], c[1:])])
-
-    if return_mesh:
-        return Mesh(nodes, connectivity, etype)
-    else:
-        return nodes, connectivity
-
-
-def rotation_matrix(alpha_deg, dim=3, axis=0):
-    "2d or 3d rotation matrix around specified axis."
-    a = np.deg2rad(alpha_deg)
-    rotation_matrix = np.array([[np.cos(a), -np.sin(a)], [np.sin(a), np.cos(a)]])
-    if dim == 3:
-        # rotation_matrix = np.pad(rotation_matrix, (1, 0))
-        # rotation_matrix[0, 0] = 1
-        rotation_matrix = np.insert(rotation_matrix, [axis], np.zeros((1, 2)), axis=0)
-        rotation_matrix = np.insert(rotation_matrix, [axis], np.zeros((3, 1)), axis=1)
-        rotation_matrix[axis, axis] = 1
-
-    return rotation_matrix
-
-
-def rotate(mesh, angle_deg, axis):
-    "Rotate mesh."
-
-    if isinstance(mesh, Mesh):
-        Nodes = mesh.nodes
-        Connectivity = mesh.connectivity
-        Etype = mesh.etype
-        return_mesh = True
-    else:
-        Nodes, Connectivity = mesh
-        return_mesh = False
-
-    dim = Nodes.shape[1]
-    nodes = (rotation_matrix(angle_deg, dim, axis) @ Nodes.T).T
-
-    if return_mesh:
-        return Mesh(nodes, Connectivity, Etype)
-    else:
-        return nodes, Connectivity
-
-
-def revolve(mesh, n=11, phi=180, axis=0):
-    "Revolve 2d quad to 3d hexahedron mesh."
-
-    if isinstance(mesh, Mesh):
-        Nodes = mesh.nodes
-        Connectivity = mesh.connectivity
-        return_mesh = True
-    else:
-        Nodes, Connectivity = mesh
-        return_mesh = False
-
-    Dim = Nodes.shape[1]
-    if Dim == 1:
-        sl = slice(None, None, -1)
-        etype = "quad"
-    elif Dim == 2:
-        sl = slice(None, None, None)
-        etype = "hexahedron"
-    else:
-        raise ValueError("Revolution of a 3d mesh is not supported.")
-
-    if abs(phi) > 360:
-        raise ValueError("phi must be within |phi| <= 360 degree.")
-
-    p = np.pad(Nodes, (0, 1))[:-1]
-    R = rotation_matrix
-
-    nodes = np.vstack(
-        [(R(alpha_deg, Dim + 1) @ p.T).T for alpha_deg in np.linspace(0, phi, n)]
-    )
-
-    c = [Connectivity + len(p) * a for a in np.arange(n)]
-
-    # if abs(phi) == 360:
-    #    nodes = nodes[: -len(p)]
-    #    c[-1] = Connectivity
-
-    connectivity = np.vstack([np.hstack((a, b[:, sl])) for a, b in zip(c[:-1], c[1:])])
-
-    nodes, connectivity = sweep((nodes, connectivity), decimals=6)
 
     if return_mesh:
         return Mesh(nodes, connectivity, etype)

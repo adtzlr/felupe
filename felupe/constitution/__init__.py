ac505ef77bcefd1d69b7e3af099e5a0a0aedc384
Andreas D <a.dutzler@gmail.com>
2021-06-28 23:41:42 +0200

    set totallagrange models as default

--- felupe/constitution/__init__.py
+++ felupe/constitution/__init__.py
@@ -1,5 +1,15 @@
 from . import df0da0
+from .df0da0 import (
+    Material,
+    Composite,
+    InvariantBased,
+    PrincipalStretchBased,
+    StrainInvariantBased,
+    Hydrostatic,
+    AsIsochoric,
+)
 from . import df_da_
 from . import df_da0
+from .df_da0 import MaterialFrom
 from .models import LinearElastic
 from . import variation

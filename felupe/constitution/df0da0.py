2dfcb12f69eb3f6564b4e064602093ce28eeba54
Andreas D <a.dutzler@gmail.com>
2021-06-28 14:24:04 +0200

    add principal stretch and invariant based models
    which both take a function (umat) as argument

--- felupe/constitution/df0da0.py
+++ felupe/constitution/df0da0.py
@@ -42,6 +42,7 @@ from ..math import (
     cdya_ik,
     cdya_il,
     det,
+    eigh,
     identity,
     trace,
     dev,
@@ -71,6 +72,151 @@ class Material:
         self.stress = stress
         self.elasticity = elasticity
         self.kind = SimpleNamespace(**{"df": 0, "da": 0})
+
+
+class InvariantBased:
+    def __init__(self, umat, tol=np.finfo(float).eps):
+        self.umat = umat
+        self.kind = SimpleNamespace(**{"df": 0, "da": 0})
+        self.C = 0
+
+    def update(self, C):
+
+        if np.all(C == self.C):
+            pass
+        else:
+            self.invariants = np.array(
+                [trace(C), (trace(C) ** 2 - trace(dot(C, C))) / 2, det(C)]
+            )
+            self.W_a, self.W_ab = self.umat(self.invariants)
+
+    def stress(self, C):
+        self.update(C)
+
+        S = np.zeros_like(C)
+
+        I1, I2, I3 = self.invariants
+        W1, W2, W3 = self.W_a
+
+        if not np.all(W1 == 0):
+            eye = identity(C)
+            S += 2 * W1 * eye
+
+        if not np.all(W2 == 0):
+            eye = identity(C)
+            S += 2 * W2 * (I1 * eye - C)
+
+        if not np.all(W3 == 0):
+            S += 2 * W3 * I3 * inv(C)
+
+        return S
+
+    def elasticity(self, C):
+        self.update(C)
+
+        ndim, ngauss, nelems = C.shape[-3:]
+        C4 = np.zeros((ndim, ndim, ndim, ndim, ngauss, nelems))
+
+        I = identity(C)
+
+        I1, I2, I3 = self.invariants
+        W1, W2, W3 = self.W_a
+        W11 = self.W_ab[0, 0]
+        W22 = self.W_ab[1, 1]
+        W33 = self.W_ab[2, 2]
+        W12 = self.W_ab[0, 1]
+        W13 = self.W_ab[0, 2]
+        W23 = self.W_ab[1, 2]
+
+        a00 = W11 + W2 + I1 * W12
+        a11 = W22
+        a22 = W33 * I3 ** 2 + W3 * I3
+        a01 = -(W12 + I1 * W22)
+        a02 = W13 * I3 + W23 * I3 * I1
+        a12 = -W23 * I3
+        b00 = -W2
+        b22 = W3 * I3
+
+        if not np.all(a00 == 0):
+            C4 += 4 * a00 * dya(I, I)
+
+        if not np.all(a11 == 0):
+            C4 += 4 * a11 * dya(C, C)
+
+        if not np.all(a22 == 0):
+            C4 += 4 * a22 * dya(inv(C), inv(C))
+
+        if not np.all(a01 == 0):
+            C4 += 4 * a01 * (dya(I, C) + dya(C, I))
+
+        if not np.all(a02 == 0):
+            C4 += 4 * a02 * (dya(I, inv(C)) + dya(inv(C), I))
+
+        if not np.all(a12 == 0):
+            C4 += 4 * a12 * (dya(C, inv(C)) + dya(inv(C), C))
+
+        if not np.all(b00 == 0):
+            C4 += 4 * b00 * cdya(I, I)
+
+        if not np.all(b22 == 0):
+            C4 += 4 * b22 * cdya(inv(C), inv(C))
+
+        return C4
+
+
+class PrincipalStretchBased:
+    def __init__(self, umat, tol=np.finfo(float).eps):
+        self.umat = umat
+        self.kind = SimpleNamespace(**{"df": 0, "da": 0})
+        self.C = 0
+        self.tol = tol
+
+    def update(self, C):
+        if np.all(C == self.C):
+            pass
+        else:
+            wC, vC = eigh(C)
+
+            self.stretches = np.sqrt(wC)
+            self.bases = np.array([dya(N, N, mode=1) for N in transpose(vC)])
+
+            self.W_a, self.W_ab = self.umat(self.stretches)
+
+    def stress(self, C):
+        self.update(C)
+
+        return np.sum(
+            [Wa / la * Ma for Wa, la, Ma in zip(self.W_a, self.stretches, self.bases)],
+            0,
+        )
+
+    def elasticity(self, C):
+        self.update(C)
+
+        ndim, ngauss, nelems = C.shape[-3:]
+        C4 = np.zeros((ndim, ndim, ndim, ndim, ngauss, nelems))
+
+        for a in range(3):
+            Wa = self.W_a[a]
+            Ma = self.bases[a]
+            la = self.stretches[a]
+
+            C4 -= Wa / la ** 3 * dya(Ma, Ma)
+
+            for b in range(3):
+                Wb = self.W_a[b]
+                Wab = self.W_ab[a, b]
+                Mb = self.bases[b]
+                lb = self.stretches[b]
+
+                C4 += Wab / la / lb * dya(Ma, Mb)
+
+                if b != a:
+                    la[abs(la - lb) < self.tol] += self.tol
+                    Gab = cdya(Ma, Mb) + cdya(Mb, Ma)
+                    C4 += (Wa / la - Wb / lb) / (la ** 2 - lb ** 2) * Gab
+
+        return C4
 
 
 class Hydrostatic:
--- felupe/constitution/df_da_.py
+++ felupe/constitution/df_da_.py
@@ -42,6 +42,7 @@ from ..math import (
     cdya_ik,
     cdya_il,
     det,
+    eigh,
     identity,
     trace,
     dev,
@@ -71,6 +72,150 @@ class Material:
         self.stress = stress
         self.elasticity = elasticity
         self.kind = SimpleNamespace(**{"df": None, "da": None})
+
+
+class InvariantBased:
+    def __init__(self, umat, tol=np.finfo(float).eps):
+        self.umat = umat
+        self.kind = SimpleNamespace(**{"df": 0, "da": 0})
+        self.b = 0
+
+    def update(self, b):
+
+        if np.all(b == self.b):
+            pass
+        else:
+            self.invariants = np.array(
+                [trace(b), (trace(b) ** 2 - trace(dot(b, b))) / 2, det(b)]
+            )
+            self.W_a, self.W_ab = self.umat(self.invariants)
+
+    def stress(self, b):
+        self.update(b)
+
+        tau = np.zeros_like(b)
+
+        I1, I2, I3 = self.invariants
+        W1, W2, W3 = self.W_a
+
+        if not np.all(W1 == 0):
+            tau += 2 * W1 * b
+
+        if not np.all(W2 == 0):
+            tau += 2 * W2 * (I1 * b - dot(b, b))
+
+        if not np.all(W3 == 0):
+            eye = identity(b)
+            tau += 2 * W3 * I3 * eye
+
+        return tau
+
+    def elasticity(self, b):
+        self.update(b)
+
+        ndim, ngauss, nelems = b.shape[-3:]
+        Jc4 = np.zeros((ndim, ndim, ndim, ndim, ngauss, nelems))
+
+        I = identity(b)
+
+        I1, I2, I3 = self.invariants
+        W1, W2, W3 = self.W_a
+        W11 = self.W_ab[0, 0]
+        W22 = self.W_ab[1, 1]
+        W33 = self.W_ab[2, 2]
+        W12 = self.W_ab[0, 1]
+        W13 = self.W_ab[0, 2]
+        W23 = self.W_ab[1, 2]
+
+        a00 = W11 + W2 + I1 * W12
+        a11 = W22
+        a22 = W33 * I3 ** 2 + W3 * I3
+        a01 = -(W12 + I1 * W22)
+        a02 = W13 * I3 + W23 * I3 * I1
+        a12 = -W23 * I3
+        b00 = -W2
+        b22 = W3 * I3
+
+        if not np.all(a00 == 0):
+            Jc4 += 4 * a00 * dya(b, b)
+
+        if not np.all(a11 == 0):
+            Jc4 += 4 * a11 * dya(dot(b, b), dot(b, b))
+
+        if not np.all(a22 == 0):
+            Jc4 += 4 * a22 * dya(I, I)
+
+        if not np.all(a01 == 0):
+            Jc4 += 4 * a01 * (dya(b, dot(b, b)) + dya(dot(b, b), b))
+
+        if not np.all(a02 == 0):
+            Jc4 += 4 * a02 * (dya(b, I) + dya(I, b))
+
+        if not np.all(a12 == 0):
+            Jc4 += 4 * a12 * (dya(dot(b, b), I) + dya(I, dot(b, b)))
+
+        if not np.all(b00 == 0):
+            Jc4 += 4 * b00 * cdya(b, b)
+
+        if not np.all(b22 == 0):
+            Jc4 += 4 * b22 * cdya(I, I)
+
+        return Jc4
+
+
+class PrincipalStretchBased:
+    def __init__(self, umat, tol=np.finfo(float).eps):
+        self.umat = umat
+        self.kind = SimpleNamespace(**{"df": 0, "da": 0})
+        self.b = 0
+        self.tol = tol
+
+    def update(self, b):
+        if np.all(b == self.b):
+            pass
+        else:
+            wb, vb = eigh(b)
+
+            self.stretches = np.sqrt(wb)
+            self.bases = np.array([dya(N, N, mode=1) for N in transpose(vb)])
+
+            self.W_a, self.W_ab = self.umat(self.stretches)
+
+    def stress(self, b):
+        self.update(b)
+
+        return np.sum(
+            [Wa * la * ma for Wa, la, ma in zip(self.W_a, self.stretches, self.bases)],
+            0,
+        )
+
+    def elasticity(self, b):
+        self.update(b)
+
+        ndim, ngauss, nelems = b.shape[-3:]
+        Jc4 = np.zeros((ndim, ndim, ndim, ndim, ngauss, nelems))
+
+        for a in range(3):
+            Wa = self.W_a[a]
+            Ma = self.bases[a]
+            la = self.stretches[a]
+
+            Jc4 -= Wa / la * dya(Ma, Ma)
+
+            for b in range(3):
+                Wb = self.W_a[b]
+                Wab = self.W_ab[a, b]
+                Mb = self.bases[b]
+                lb = self.stretches[b]
+
+                Jc4 += Wab * dya(Ma, Mb)
+
+                if b != a:
+                    la[abs(la - lb) < self.tol] += self.tol
+                    Gab = cdya(Ma, Mb) + cdya(Mb, Ma)
+                    Jc4 += (Wa * la - Wb * lb) / (la ** 2 - lb ** 2) * Gab
+
+        return Jc4
 
 
 class Hydrostatic:

f75ccd7446bdc730b2eb8b87597d6426fbe003be
Andreas D <a.dutzler@gmail.com>
2021-07-06 15:00:47 +0200

    revert reshape of bilinear form integrate()( result

--- felupe/forms.py
+++ felupe/forms.py
@@ -190,13 +190,9 @@ class IntegralForm:
         else:
 
             if not grad_v and not grad_u:
-                values = np.einsum(
+                return np.einsum(
                     "ape,...pe,bpe,pe->a...be", vb, fun, ub, dV, optimize=True
                 )
-                if len(values.shape) > 3:
-                    a, i, b, e = values.shape
-                    values = values.reshape(a, i, b, i, e)
-                return values
             elif grad_v and not grad_u:
                 if parallel:
                     return integrate_gradv_u(vb, fun, ub, dV)

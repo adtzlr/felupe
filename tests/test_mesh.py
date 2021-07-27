29122d2800c98c0b4c049ceffa71edb03d830acc
Andreas D <a.dutzler@gmail.com>
2021-07-27 10:13:53 +0200

    Update test_mesh.py

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -28,15 +28,23 @@ import pytest
 import felupe as fe
 
 
-def test_mesh():
-    mr1 = fe.mesh.Rectangle()
-    mr2 = fe.mesh.Rectangle(a=(-1.2, -2), b=(2, 3.1), n=(4, 9))
-    mr3 = fe.mesh.Rectangle(n=7)
+def test_meshes():
+    
+    m = fe.mesh.Line(n=5)
+    assert m.points.shape == (5, 1)
+    assert m.cells.shape == (4, 2)
+    
+    m = fe.mesh.Rectangle(a=(-1.2, -2), b=(2, 3.1), n=(4, 9))
+    assert m.points.shape == (4 * 9, 2)
+    assert m.cells.shape == (3 * 8, 4)
 
-    mc1 = fe.mesh.Cube()
-    mc2 = fe.mesh.Cube(a=(-1, -2, -0.5), b=(2, 3.1, 1), n=(4, 9, 5))
-    mc3 = fe.mesh.Cube(n=7)
-
+    m = fe.mesh.Cube(a=(-1, -2, -0.5), b=(2, 3.1, 1), n=(4, 9, 5))
+    assert m.points.shape == (4 * 9 * 5, 3)
+    assert m.cells.shape == (3 * 8 * 4, 8)
+    
+    m = fe.mesh.Cylinder(n = (3,9,3), phi=180)
+    assert m.points.shape == (3 * 9 * 3, 3)
+    assert m.cells.shape == (2 * 8 * 2, 8)
 
 if __name__ == "__main__":
-    test_mesh()
+    test_meshes()

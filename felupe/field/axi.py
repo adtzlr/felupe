4d9a3d7204346fd4033bd74a10ec28e57b2e5acd
Andreas D <a.dutzler@gmail.com>
2021-09-16 08:36:43 +0200

    Update axi.py

--- felupe/field/axi.py
+++ felupe/field/axi.py
@@ -33,7 +33,7 @@ along with Felupe.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
 from .base import Field
-from .math import sym as symmetric
+from ..math import sym as symmetric
 
 
 class FieldAxisymmetric(Field):

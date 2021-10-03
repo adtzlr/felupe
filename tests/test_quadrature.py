bc7e396d0aea0eb1f97189399e7345b7578d3ed3
Andreas D <a.dutzler@gmail.com>
2021-10-03 14:26:44 +0200

    Update test_quadrature.py

--- tests/test_quadrature.py
+++ tests/test_quadrature.py
@@ -25,51 +25,51 @@ along with Felupe.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 import pytest
-import numpy as np
+
 import felupe as fe
 
 
 def test_gausslegendre():
-    q11 = fe.quadrature.GaussLegendre(order=1, dim=1)
+    q11 = fe.GaussLegendre(order=1, dim=1)
     assert q11.points.shape == (2, 1)
     assert q11.weights.sum() == 2
 
-    q12 = fe.quadrature.GaussLegendre(order=1, dim=2)
+    q12 = fe.GaussLegendre(order=1, dim=2)
     assert q12.points.shape == (4, 2)
     assert q12.weights.sum() == 4
 
-    q13 = fe.quadrature.GaussLegendre(order=1, dim=3)
+    q13 = fe.GaussLegendre(order=1, dim=3)
     assert q13.points.shape == (8, 3)
     assert q13.weights.sum() == 8
 
     with pytest.raises(ValueError):
-        fe.quadrature.GaussLegendre(order=1, dim=4)
+        fe.GaussLegendre(order=1, dim=4)
 
 
 def test_triangle():
-    q = fe.quadrature.Triangle(order=1)
+    q = fe.TriangleQuadrature(order=1)
     assert q.points.shape == (1, 2)
     assert q.weights.sum() == 1 / 2
 
-    q = fe.quadrature.Triangle(order=2)
+    q = fe.TriangleQuadrature(order=2)
     assert q.points.shape == (3, 2)
     assert q.weights.sum() == 1 / 2
 
-    q = fe.quadrature.Triangle(order=3)
+    q = fe.TriangleQuadrature(order=3)
     assert q.points.shape == (4, 2)
     assert q.weights.sum() == 1 / 2
 
 
 def test_tetra():
-    q = fe.quadrature.Tetrahedron(order=1)
+    q = fe.TetrahedronQuadrature(order=1)
     assert q.points.shape == (1, 3)
     assert q.weights.sum() == 1 / 6
 
-    q = fe.quadrature.Tetrahedron(order=2)
+    q = fe.TetrahedronQuadrature(order=2)
     assert q.points.shape == (4, 3)
     assert q.weights.sum() == 1 / 6
 
-    q = fe.quadrature.Tetrahedron(order=3)
+    q = fe.TetrahedronQuadrature(order=3)
     assert q.points.shape == (5, 3)
     assert q.weights.sum() == 1 / 6
 

f40bf970d9e4c5cb4c86e4d0dfc9a9a0b00b158a
Andreas D <a.dutzler@gmail.com>
2021-10-10 12:31:39 +0200

    Update _region.py
    according to changes in elements

--- felupe/region/_region.py
+++ felupe/region/_region.py
@@ -40,7 +40,7 @@ class Region:
         all integration points of each cell in the region if the
         optional argument `grad` is True (default is True).
         """
-        
+
         self.mesh = mesh
         self.element = element
         self.quadrature = quadrature
@@ -48,34 +48,36 @@ class Region:
         # element shape function "a" evaluated at quadrature point "p"
         #
         # h_ap
-        self.h = np.array([self.element.shape(p) for p in self.quadrature.points]).T
+        self.h = np.array(
+            [self.element.shape.function(p) for p in self.quadrature.points]
+        ).T
 
         # partial derivative of element shape function "a"
         # w.r.t. natural element coordinate "J" evaluated at quadrature point "p"
         #
         # dhdr_aJp
         self.dhdr = np.array(
-            [self.element.shape_derivative(p) for p in self.quadrature.points]
+            [self.element.shape.gradient(p) for p in self.quadrature.points]
         ).transpose(1, 2, 0)
 
-        if self.element.nshape > 1 and self.mesh.dim == self.element.dim and grad:
+        if grad:
 
             # geometric gradient as partial derivative of undeformed coordinate "I"
             # w.r.t. natural element coordinate "J" evaluated at quadrature point "p"
-            # for every cell "c" (geometric gradient or 
+            # for every cell "c" (geometric gradient or
             # **Jacobian** transformation between "X" and "r")
-            # 
+            #
             # dXdr_IJpe
             self.dXdr = np.einsum(
                 "caI,aJp->IJpc", self.mesh.points[self.mesh.cells], self.dhdr
             )
-            
+
             # inverse of dXdr
             self.drdX = inv(self.dXdr)
 
             # Determinant of geometric gradient evaluated at quadrature point "p"
             # for every cell "c" multiplied by corresponding quadrature weight
-            # according to integration point "p", denoted as 
+            # according to integration point "p", denoted as
             # "differential volume element"
             #
             # dV_pc = det(dXdr)_pc * w_p

bdac50cba877f600e0fd2c6c5e759877ed27d177
Andreas D <a.dutzler@gmail.com>
2021-11-26 22:59:06 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -29,6 +29,7 @@ author = 'Andreas Dutzler'
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.

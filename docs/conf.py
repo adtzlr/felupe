6f3b3c7a08f599ccc61743c210d33d1a01181864
Andreas D <a.dutzler@gmail.com>
2021-11-26 18:36:07 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,6 @@ author = 'Andreas Dutzler'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

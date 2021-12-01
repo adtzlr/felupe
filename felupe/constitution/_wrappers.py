690853c21dc6f075005a427d6ee2561b41eb93c7
Andreas D <a.dutzler@gmail.com>
2021-12-01 21:16:30 +0100

    add docstrings to wrappers

--- felupe/constitution/_wrappers.py
+++ felupe/constitution/_wrappers.py
@@ -29,12 +29,40 @@ from multiprocessing import cpu_count
 
 
 class MatadiMaterial:
+    """Wrap a matADi Materials into a FElupe material. While matADi always
+    requires and returns a list of input parameters / return values, FElupe
+    takes one or more arguments and returns an array or a list of arrays.
+
+    Arguments
+    ---------
+    material : matadi.MaterialHyperelastic
+        Hyperelastic material from matADi
+    threads : int, optional
+        Number of threads (default is number of CPU cores)
+
+    """
+
     def __init__(self, material, threads=cpu_count()):
-        "Wrap a MatADi Materials into a FElupe material."
+
         self.material = material
         self.threads = threads
 
     def function(self, *args):
+        """Evaluate the function.
+
+        Arguments
+        ---------
+        *args : tuple
+            Input arguments
+
+        Returns
+        -------
+        list or ndarray
+            The evaluated function either as array if one input argument is
+            passed or as list of arrays if more than one input arguments are
+            provided.
+
+        """
         fun = self.material.function(args, threads=self.threads)
         if len(fun) == 1:
             return fun[0]
@@ -42,6 +70,21 @@ class MatadiMaterial:
             return fun
 
     def gradient(self, *args):
+        """Evaluate the gradient.
+
+        Arguments
+        ---------
+        *args : tuple
+            Input arguments
+
+        Returns
+        -------
+        list or ndarray
+            The evaluated gradient either as array if one input argument is
+            passed or as list of arrays if more than one input arguments are
+            provided.
+
+        """
         grad = self.material.gradient(args, threads=self.threads)
         if len(grad) == 1:
             return grad[0]
@@ -49,6 +92,21 @@ class MatadiMaterial:
             return grad
 
     def hessian(self, *args):
+        """Evaluate the hessian.
+
+        Arguments
+        ---------
+        *args : tuple
+            Input arguments
+
+        Returns
+        -------
+        list or ndarray
+            The evaluated hessian either as array if one input argument is
+            passed or as list of arrays if more than one input arguments are
+            provided.
+
+        """
         hess = self.material.hessian(args, threads=self.threads)
         if len(hess) == 1:
             return hess[0]

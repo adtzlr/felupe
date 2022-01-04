fe3e067fc96d1cf0fc9d7cf34cc464ad5f92bc39
Andreas D <a.dutzler@gmail.com>
2022-01-04 20:59:22 +0100

    lint black

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,15 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'FElupe'
-copyright = '2021, Andreas Dutzler'
-author = 'Andreas Dutzler'
+project = "FElupe"
+copyright = "2021, Andreas Dutzler"
+author = "Andreas Dutzler"
 
 
 # -- General configuration ---------------------------------------------------
@@ -36,12 +37,12 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,7 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = "furo"
 html_title = "FElupe"
 
 html_static_path = ["_static"]
@@ -61,4 +62,4 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]

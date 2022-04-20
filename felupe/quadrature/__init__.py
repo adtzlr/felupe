822db63e44d6f4521ec1b84a011deb406abf5cd1
adtzlr <a.dutzler@gmail.com>
2022-04-20 13:52:50 +0200

    add gauss-legendre quadrature on boundaries

--- felupe/quadrature/__init__.py
+++ felupe/quadrature/__init__.py
@@ -1,4 +1,7 @@
 from ._base import Scheme
 from ._triangle import Triangle
 from ._tetra import Tetrahedron
-from ._gausslegendre import GaussLegendre
+from ._gausslegendre import (
+    GaussLegendre,
+    GaussLegendreBoundary,
+)

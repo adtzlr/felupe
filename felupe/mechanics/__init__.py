a4ee77b4e1e1066b4f65606d381646ad6c2a8987
Andreas Dutzler <a.dutzler@gmail.com>
2022-08-08 13:59:29 +0200

    modernize multipoint
    move to mechanics, use assemble helper and store results

--- felupe/mechanics/__init__.py
+++ felupe/mechanics/__init__.py
@@ -2,3 +2,10 @@ from ._solidbody import SolidBody
 from ._solidbody_pressure import SolidBodyPressure
 from ._solidbody_tensor import SolidBodyTensor
 from ._solidbody_gravity import SolidBodyGravity
+try:
+    from ._multipoint import (
+        MultiPointConstraint,
+        MultiPointContact,
+    )
+except:
+    pass

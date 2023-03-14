caff7f4041d53d0e8f79d5dcc518d633bb6821e6
Andreas Dutzler <a.dutzler@gmail.com>
2023-03-14 23:21:08 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -58,8 +58,3 @@ html_theme_options = {
     "light_logo": "logo_light.svg",
     "dark_logo": "logo_dark.svg",
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]

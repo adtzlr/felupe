2933d794e0b4e3ebdb31057dadfe927e127f69f2
Andreas Dutzler <a.dutzler@gmail.com>
2023-04-02 01:32:30 +0200

    Vectorize `MultiPointContact`

--- src/felupe/mechanics/_multipoint.py
+++ src/felupe/mechanics/_multipoint.py
@@ -26,7 +26,6 @@ along with Felupe.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import numpy as np
-import sparse
 from scipy.sparse import eye, lil_matrix
 
 from ._helpers import Assemble, Results
@@ -104,51 +103,58 @@ class MultiPointContact:
 
     def _vector(self, field=None, parallel=False):
         "Calculate vector of residuals with RBE2 contributions."
+
         if field is not None:
             self.field = field
-        f = self.field.fields[0]
-        r = sparse.DOK(shape=(self.mesh.npoints, self.mesh.dim))
-        c = self.centerpoint
-        for t in self.points:
-            for d in self.axes:
-                Xc = self.mesh.points[c, d]
-                Xt = self.mesh.points[t, d]
-                xc = f.values[c, d] + Xc
-                xt = f.values[t, d] + Xt
-                if np.sign(-Xt + Xc) != np.sign(-xt + xc):
-                    n = -xt + xc
-                    r[t, d] = -self.multiplier * n
-                    r[c, d] += self.multiplier * n
-        self.results.force = sparse.COO(r).reshape((-1, 1)).tocsr()
+
+        u = self.field.fields[0].values
+
+        Xc = self.mesh.points[self.centerpoint]
+        Xt = self.mesh.points[self.points]
+
+        xc = u[self.centerpoint] + Xc
+        xt = u[self.points] + Xt
+
+        mask = np.sign(-Xt + Xc) == np.sign(-xt + xc)
+        n = -xt + xc
+        n[mask] = 0
+
+        r = lil_matrix(u.shape)
+        r[self.points] = -self.multiplier * n
+        r[self.centerpoint] = self.multiplier * n.sum(axis=0)
+
+        self.results.force = r.reshape(-1, 1).tocsr()
         return self.results.force
 
     def _matrix(self, field=None, parallel=False):
         "Calculate stiffness with RBE2 contributions."
+
         if field is not None:
             self.field = field
-        f = self.field.fields[0]
-        L = sparse.DOK(
-            shape=(self.mesh.npoints, self.mesh.dim, self.mesh.npoints, self.mesh.dim)
-        )
-        c = self.centerpoint
-        for t in self.points:
-            for d in self.axes:
-                Xc = self.mesh.points[c, d]
-                Xt = self.mesh.points[t, d]
-                xc = f.values[c, d] + Xc
-                xt = f.values[t, d] + Xt
-                # n = 0
-                if np.sign(-Xt + Xc) != np.sign(-xt + xc):
-                    # n = -xt + xc
-                    L[t, d, t, d] = self.multiplier
-                    L[t, d, c, d] = -self.multiplier
-                    L[c, d, t, d] = -self.multiplier
-                    L[c, d, c, d] += self.multiplier
-        self.results.stiffness = (
-            sparse.COO(L)
-            .reshape(
-                (self.mesh.npoints * self.mesh.dim, self.mesh.npoints * self.mesh.dim)
-            )
-            .tocsr()
-        )
+
+        u = self.field.fields[0].values
+
+        Xc = self.mesh.points[self.centerpoint]
+        Xt = self.mesh.points[self.points]
+
+        xc = u[self.centerpoint] + Xc
+        xt = u[self.points] + Xt
+
+        mask = np.sign(-Xt + Xc) != np.sign(-xt + xc)
+        masks = [mask[:, ax] for ax in self.axes]
+
+        indices = np.arange(self.mesh.ndof).reshape(self.mesh.points.shape)
+        td = [indices[self.points.reshape(-1, 1), ax].ravel() for ax in self.axes]
+        cd = [indices[self.centerpoint, ax].ravel() for ax in self.axes]
+
+        L = lil_matrix((self.mesh.ndof, self.mesh.ndof))
+
+        for t, c, m in zip(td, cd, masks):
+
+            L[t[m].reshape(-1, 1), t[m]] = eye(len(t[m])) * self.multiplier
+            L[t[m].reshape(-1, 1), c] = -self.multiplier
+            L[c.reshape(-1, 1), t[m]] = -self.multiplier
+            L[c.reshape(-1, 1), c] = eye(len(c)) * self.multiplier * len(self.points[m])
+
+        self.results.stiffness = L.tocsr()
         return self.results.stiffness

c1242227b4ff7805e7d03a3d795127877e3dc0e1
Andreas Dutzler <a.dutzler@gmail.com>
2023-04-16 17:05:07 +0200

    Flake8: Fix mechanics

--- src/felupe/mechanics/_job.py
+++ src/felupe/mechanics/_job.py
@@ -1,28 +1,19 @@
 # -*- coding: utf-8 -*-
 """
- _______  _______  ___      __   __  _______  _______ 
-|       ||       ||   |    |  | |  ||       ||       |
-|    ___||    ___||   |    |  | |  ||    _  ||    ___|
-|   |___ |   |___ |   |    |  |_|  ||   |_| ||   |___ 
-|    ___||    ___||   |___ |       ||    ___||    ___|
-|   |    |   |___ |       ||       ||   |    |   |___ 
-|___|    |_______||_______||_______||___|    |_______|
+This file is part of FElupe.
 
-This file is part of felupe.
-
-Felupe is free software: you can redistribute it and/or modify
+FElupe is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Felupe is distributed in the hope that it will be useful,
+FElupe is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Felupe.  If not, see <http://www.gnu.org/licenses/>.
-
+along with FElupe.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from platform import architecture, machine, platform
@@ -116,7 +107,7 @@ FElupe Version {version} ({platform(terse=True)} {machine()} {architecture()[0]}
             print("=======\n")
 
         if parallel:
-            if not "kwargs" in kwargs.keys():
+            if "kwargs" not in kwargs.keys():
                 kwargs["kwargs"] = {}
             kwargs["kwargs"]["parallel"] = True
 

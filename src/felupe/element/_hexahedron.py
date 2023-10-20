b1ddef80a7a0cf802c5f566d698ee145d4603a77
Andreas Dutzler <a.dutzler@gmail.com>
2023-10-21 00:11:09 +0200

    Update _hexahedron.py

--- src/felupe/element/_hexahedron.py
+++ src/felupe/element/_hexahedron.py
@@ -42,6 +42,7 @@ class ConstantHexahedron(Element):
     .. [1] W. Schroeder, K. Martin and B. Lorensen. The Visualization
        Toolkit, 4th ed. Kitware, 2006. ISBN: 978-1-930934-19-1.
     """
+
     def __init__(self):
         self.points = np.array(
             [
@@ -153,6 +154,26 @@ class Hexahedron(Element):
 
 
 class QuadraticHexahedron(Element):
+    r"""A 3D hexahedron (brick) element formulation with quadratic (serendipity) shape
+    functions.
+
+    Notes
+    -----
+    The hexahedron element is defined by twenty points with eight corner points (0-7)
+    where (0,1,2,3) forms the base and (4,5,6,7) the opposite quad; followed by 12 mid-
+    edge points. The mid-edge points correspond to the edges defined by the lines
+    between the points (0,1), (1,2), (2,3), (3,0), (4,5), (5,6), (6,7), (7,4), (0,4),
+    (1,5), (2,6), (3,7). [1]_
+
+    The shape functions :math:`\boldsymbol{h}` are given in terms of the coordinates
+    :math:`(r,s,t)`.
+
+    References
+    ----------
+    .. [1] W. Schroeder, K. Martin and B. Lorensen. The Visualization
+       Toolkit, 4th ed. Kitware, 2006. ISBN: 978-1-930934-19-1.
+    """
+
     def __init__(self):
         self.points = np.array(
             [
@@ -186,7 +207,7 @@ class QuadraticHexahedron(Element):
         super().__init__(shape=(20, 3))
 
     def function(self, rst):
-        "quadratic serendipity hexahedron shape functions"
+        "Return the shape functions at given coordinates (r, s, t)."
         r, s, t = rst
         return np.array(
             [
@@ -218,7 +239,7 @@ class QuadraticHexahedron(Element):
         )
 
     def gradient(self, rst):
-        "quadratic serendipity hexahedron gradient of shape functions"
+        "Return the gradient of shape functions at given coordinates (r, s, t)."
         r, s, t = rst
         return np.array(
             [
@@ -355,6 +376,31 @@ class QuadraticHexahedron(Element):
 
 
 class TriQuadraticHexahedron(Element):
+    r"""A 3D hexahedron (brick) element formulation with tri-quadratic shape functions.
+
+    Notes
+    -----
+    The hexahedron element is defined by 27 points. This includes 8 corner points, 12
+    mid-edge points, 6 mid-face points and one mid-volume point. The ordering of the 27
+    points defining the element is point ids (0-7,8-19, 20-25, 26) where point ids 0-7
+    are the eight corner vertices of the cube; followed by twelve midedge points (8-19);
+    followed by 6 mid-face points (20-25) and the last point (26) is the mid-volume
+    point. Note that these mid-edge points correspond to the edges defined by (0,1),
+    (1,2), (2,3), (3,0), (4,5), (5,6), (6,7), (7,4), (0,4), (1,5), (2,6), (3,7). The
+    mid-surface points lie on the faces defined by (first edge point id's, than mid-edge
+    point id's): (0,1,5,4;8,17,12,16), (1,2,6,5;9,18,13,17), (2,3,7,6,10,19,14,18),
+    (3,0,4,7;11,16,15,19), (0,1,2,3;8,9,10,11), (4,5,6,7;12,13,14,15). The last point
+    lies in the center (0,1,2,3,4,5,6,7). [1]_
+
+    The shape functions :math:`\boldsymbol{h}` are given in terms of the coordinates
+    :math:`(r,s,t)`.
+
+    References
+    ----------
+    .. [1] W. Schroeder, K. Martin and B. Lorensen. The Visualization
+       Toolkit, 4th ed. Kitware, 2006. ISBN: 978-1-930934-19-1.
+    """
+
     def __init__(self):
         super().__init__(shape=(27, 3))
 
@@ -409,11 +455,11 @@ class TriQuadraticHexahedron(Element):
         )
 
     def function(self, rst):
-        "quadratic hexahedron shape functions"
+        "Return the shape functions at given coordinates (r, s, t)."
 
         return self._lagrange.function(rst)[self._permute]
 
     def gradient(self, rst):
-        "quadratic hexahedron gradient of shape functions"
+        "Return the gradient of shape functions at given coordinates (r, s, t)."
 
         return self._lagrange.gradient(rst)[self._permute, :]

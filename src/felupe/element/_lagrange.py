e4c982776ae8f59c20890024ea5637b088e6c68f
Andreas Dutzler <a.dutzler@gmail.com>
2023-10-21 15:55:37 +0200

    Update _lagrange.py

--- src/felupe/element/_lagrange.py
+++ src/felupe/element/_lagrange.py
@@ -26,7 +26,82 @@ from ._base import Element
 
 
 class ArbitraryOrderLagrange(Element):
-    "Lagrange quad/hexahdron finite element of arbitrary order."
+    r"""A n-dimensional Lagrange finite element (e.g. line, quad or hexahdron) of
+    arbitrary order.
+
+    Notes
+    -----
+
+    **Polynomial shape functions**
+
+    The basis function vector is generated with row-stacking of the individual lagrange
+    polynomials. Each polynomial defined in the interval :math:`[-1,1]` is a function of
+    the parameter :math:`r`. The curve parameters matrix :math:`\boldsymbol{A}` is of
+    symmetric shape due to the fact that for each evaluation point :math:`r_j` exactly
+    one basis function :math:`h_j(r)` is needed.
+
+    ..  math::
+
+        \boldsymbol{h}(r) = \boldsymbol{A}^T \boldsymbol{r}(r)
+
+    **Curve parameter matrix**
+
+    The evaluation of the curve parameter matrix :math:`\boldsymbol{A}` is carried out
+    by boundary conditions. Each shape function :math:`h_i` has to take the value of one
+    at the associated nodal coordinate :math:`r_i` and zero at all other point
+    coordinates.
+
+    ..  math::
+
+        \boldsymbol{A}^T \boldsymbol{R} &=
+            \boldsymbol{I} \qquad \text{with} \qquad \boldsymbol{R} =
+            \begin{bmatrix}\boldsymbol{r}(r_1) & \boldsymbol{r}(r_2) & \dots &
+            \boldsymbol{r}(r_p)\end{bmatrix}
+
+        \boldsymbol{A}^T &= \boldsymbol{R}^{-1}
+
+
+    **Interpolation and partial derivatives**
+
+    The approximation of nodal unknowns :math:`\hat{\boldsymbol{u}}` as a function of
+    the parameter :math:`r` is evaluated as
+
+    ..  math::
+
+        \boldsymbol{u}(r) \approx \hat{\boldsymbol{u}}^T \boldsymbol{h}(r)
+
+    For the calculation of the partial derivative of the interpolation field w.r.t. the
+    parameter :math:`r` a simple shift of the entries of the parameter vector is enough.
+    This shifted parameter vector is denoted as :math:`\boldsymbol{r}^-`. A minus
+    superscript indices the negative shift of the vector entries by :math:`-1`.
+
+    ..  math::
+
+        \frac{\partial \boldsymbol{u}(r)}{\partial r} &\approx
+            \hat{\boldsymbol{u}}^T \frac{\partial \boldsymbol{h}(r)}{\partial r}
+
+        \frac{\partial \boldsymbol{h}(r)}{\partial r} &=
+            \boldsymbol{A}^T \boldsymbol{r}^-(r) \qquad \text{with} \qquad r_0^- =
+            0 \qquad \text{and} \qquad r_i^- = \frac{r^{(i-1)}}{(i-1)!} \qquad
+            \text{for} \qquad  i=(1 \dots p)
+
+
+    n-dimensional shape functions
+    *****************************
+
+    Multi-dimensional shape function matrices
+    :math:`\boldsymbol{H}_{2D}, \boldsymbol{H}_{3D}` are simply evaluated as dyadic
+    (outer) vector products of one-dimensional shape function vectors. The multi-
+    dimensional shape function vector is a one-dimensional representation (flattened
+    version) of the multi-dimensional shape function matrix.
+
+    ..  math::
+
+        \boldsymbol{H}_{2D}(r,s) &= \boldsymbol{h}(r) \otimes \boldsymbol{h}(s)
+
+        \boldsymbol{H}_{3D}(r,s,t) &= \boldsymbol{h}(r) \otimes \boldsymbol{h}(s)
+            \otimes \boldsymbol{h}(t)
+    """
 
     def __init__(self, order, dim, interval=(-1, 1)):
         self._order = order
@@ -54,7 +129,7 @@ class ArbitraryOrderLagrange(Element):
         self.points = np.vstack([point.ravel() for point in grid]).T
 
     def function(self, r):
-        "Shape function vector at coordinate vector r."
+        "Return the shape functions at given coordinate vector r."
         n = self._nshape
 
         # 1d - basis function vectors per axis
@@ -63,7 +138,7 @@ class ArbitraryOrderLagrange(Element):
         return np.einsum(self._subscripts, *h).ravel("F")
 
     def gradient(self, r):
-        "Gradient of shape function vector at coordinate vector r."
+        "Return the grgadient of shape functions at given coordinate vector r."
         n = self._nshape
 
         # 1d - basis function vectors per axis
@@ -84,10 +159,10 @@ class ArbitraryOrderLagrange(Element):
         return dhdr
 
     def _points(self, n):
-        "Equidistant n points in interval [-1, 1]."
+        "Equidistant n-points in interval [-1, 1]."
         return np.linspace(*self._interval, n)
 
     def _polynomial(self, r, n):
-        "Lagrange-Polynomial of order n evaluated at coordinate r."
+        "Lagrange-Polynomial of order n evaluated at coordinate vector r."
         m = np.arange(n)
         return r**m / factorial(m)

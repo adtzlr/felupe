b6cf4dbafd16b89e898f4500354f9cea0392ad22
Andreas Dutzler <a.dutzler@gmail.com>
2024-02-11 11:33:36 +0100

    Update _solidbody_gravity.py

--- src/felupe/mechanics/_solidbody_gravity.py
+++ src/felupe/mechanics/_solidbody_gravity.py
@@ -24,7 +24,51 @@ from ._helpers import Assemble, Results
 
 
 class SolidBodyGravity:
-    "A SolidBody with methods for the assembly of sparse vectors/matrices."
+    """A gravity (body) force on a solid body.
+
+    Parameters
+    ----------
+    field : FieldContainer
+        A field container with fields created on a boundary region.
+    gravity : ndarray or None, optional
+        The prescribed values of gravity :math:`\boldsymbol{g}` (default is None). If
+        None, the gravity vector is set to zero (the dimension of the gravity vector is
+        derived from the first field of the field container). If necessary, the array is
+        trimmed to the dimension of the first field of the field container.
+    density : float, optional
+        The density :math:`\rho` of the solid body (default is 1.0).
+
+    Notes
+    -----
+    ..  math::
+
+        \delta W_{ext} = \int_V
+            \delta \boldsymbol{u} \cdot \rho \boldsymbol{g} \ dV
+
+    Examples
+    --------
+    >>> import felupe as fem
+    >>>
+    >>> mesh = fem.Cube(n=6)
+    >>> region = fem.RegionHexahedron(mesh)
+    >>> field = fem.FieldContainer([fem.Field(region, dim=3)])
+    >>> boundaries = fem.dof.symmetry(field[0])
+    >>>
+    >>> umat = fem.NeoHooke(mu=1, bulk=2)
+    >>> solid = fem.SolidBody(umat, field)
+    >>> gravity = fem.SolidBodyGravity(field, density=1.0)
+    >>>
+    >>> table = fem.math.linsteps([0, 1], num=5, axis=0, axes=3)
+    >>> step = fem.Step(
+    >>>     items=[solid, gravity],
+    >>>     ramp={gravity: 2 * table},
+    >>>     boundaries=boundaries,
+    >>> )
+    >>>
+    >>> job = fem.Job(steps=[step]).evaluate()
+    >>> ax = solid.imshow("Principal Values of Cauchy Stress")
+
+    """
 
     def __init__(self, field, gravity=None, density=1.0):
         self.field = field
@@ -32,7 +76,10 @@ class SolidBodyGravity:
         self.assemble = Assemble(vector=self._vector, matrix=self._matrix)
         self._form = IntegralForm
 
-        self.results.gravity = np.array(gravity)
+        self.results.gravity = np.zeros(self.field[0].dim)
+        if gravity is not None:
+            self.results.gravity = np.array(gravity)[: self.field[0].dim]
+
         self.results.density = density
 
     def update(self, gravity):

c65774c6708b955109d28129591dc94d05dea4f9
Andreas Dutzler <a.dutzler@gmail.com>
2024-02-27 16:18:17 +0100

    Update _tensor.py

--- src/felupe/math/_tensor.py
+++ src/felupe/math/_tensor.py
@@ -353,7 +353,7 @@ def det(A, out=None):
     ..  math::
         :label: math-det
 
-        \det \left( \boldsymbol{A} \right) &=
+        \det \left( \boldsymbol{A} \right) =
             A_{11}~A_{22}~A_{33} +
             A_{12}~A_{23}~A_{31} +
             A_{13}~A_{21}~A_{32} -

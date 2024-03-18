661b9792478fb2a06c76d55b3672ff322cd7317e
Andreas Dutzler <a.dutzler@gmail.com>
2024-03-18 20:54:25 +0100

    Aktualisieren von test_mechanics.py

--- tests/test_mechanics.py
+++ tests/test_mechanics.py
@@ -500,14 +500,24 @@ def test_load():
 
 
 def test_view():
-    mesh = fem.Rectangle(n=6)
+    mesh = fem.Rectangle(n=3)
     region = fem.RegionQuad(mesh)
+
     field = fem.FieldContainer([fem.FieldPlaneStrain(region, dim=2)])
     umat = fem.NeoHooke(mu=1, bulk=2)
     solid = fem.SolidBody(umat, field)
     plotter = solid.plot(off_screen=True)
     # img = solid.screenshot(transparent_background=True)
     # ax = solid.imshow()
+
+    field = fem.FieldContainer([fem.Field(region, dim=2)])
+    umat = Fem.LinearElasticPlaneStress(E=1, nu=0.3)
+
+    solid = fem.SolidBody(umat, field)
+    plotter = solid.plot(off_screen=True)
+
+    solid = fem.SolidBodyNearlyIncompressible(umat, field, bulk=0)
+    plotter = solid.plot(off_screen=True)
 
 
 def test_threefield():

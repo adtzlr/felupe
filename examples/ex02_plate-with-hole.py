833eb11ad6203c4fc4fbaefaba9b016abae6c3b8
Andreas Dutzler <a.dutzler@gmail.com>
2024-03-18 21:50:46 +0100

    Update ex02_plate-with-hole.py

--- examples/ex02_plate-with-hole.py
+++ examples/ex02_plate-with-hole.py
@@ -41,7 +41,7 @@ line = fem.mesh.Line(n=21)
 curve = line.copy(points=r * np.vstack([np.cos(phi), np.sin(phi)]).T)
 top = line.copy(points=np.vstack([np.linspace(0, h, 21), np.linspace(h, h, 21)]).T)
 
-face = curve.fill_between(top, n=np.linspace(0, 1, 21)**1.3 * 2 - 1)
+face = curve.fill_between(top, n=np.linspace(0, 1, 21) ** 1.3 * 2 - 1)
 rect = fem.mesh.Rectangle(a=(h, 0), b=(L, h), n=21)
 mesh = fem.mesh.concatenate([face, face.mirror(normal=[-1, 1, 0]), rect])
 mesh = mesh.sweep(decimals=5)
@@ -76,19 +76,19 @@ load = fem.SolidBodyPressure(field_boundary, pressure=-100)
 
 # %%
 # The simulation model is now ready to be solved. The equivalent von Mises Cauchy stress
-# will be plotted. For the two-dimensional case it is calculated by:
+# will be plotted. For the two-dimensional case it is calculated by Eq. :eq:`svM`.
+# Stresses, located at quadrature-points of cells, are shifted to and averaged at mesh-
+# points.
 #
 # .. math::
+#    :label: svM
 #
 #    \sigma_{vM} = \sqrt{\sigma_{11}^2 + \sigma_{22}^2 - \sigma_{11} \ \sigma_{22}
 #                + 3 \ \sigma_{12}^2}
 step = fem.Step(items=[solid, load], boundaries=boundaries)
 job = fem.Job(steps=[step]).evaluate()
 
-s = solid.evaluate.cauchy_stress()
-svM = np.sqrt(s[0, 0] ** 2 + s[1, 1] ** 2 - s[0, 0] * s[1, 1] + 3 * s[0, 1] ** 2)
-plotter = field.view(point_data={"Cauchy Stress (von Mises)": fem.project(svM, region)})
-plotter.plot("Cauchy Stress (von Mises)", show_edges=False).show()
+solid.plot("Equivalent of Cauchy Stress", show_edges=False, project=fem.topoints).show()
 
 # %%
 # The normal stress distribution :math:`\sigma_{11}` over the hole at :math:`x=0` is

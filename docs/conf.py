2ab1e1082317ed6256f707b0d9a0f99ff382596b
Andreas Dutzler <a.dutzler@gmail.com>
2024-04-20 20:38:06 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import datetime
 import os
 import sys
 
@@ -38,7 +39,8 @@ os.environ["FELUPE_VERBOSE"] = "false"
 # -- Project information -----------------------------------------------------
 
 project = "FElupe"
-copyright = "2024, Andreas Dutzler"
+year = datetime.date.today().year
+copyright = f"2021-{year}, Andreas Dutzler"
 author = "Andreas Dutzler"
 
 

1815f5363c54b1e446b12f6e7715e83175da1896
Andreas Dutzler <a.dutzler@gmail.com>
2024-05-10 22:18:24 +0200

    Update _miehe_goektepe_lulei.py

--- src/felupe/constitution/hyperelasticity/models/_miehe_goektepe_lulei.py
+++ src/felupe/constitution/hyperelasticity/models/_miehe_goektepe_lulei.py
@@ -1,6 +1,3 @@
-from tensortrax.math.linalg import det
-
-from ....quadrature import BazantOh
 from .microsphere import langevin, linear, nonaffine_stretch, nonaffine_tube
 
 
@@ -66,8 +63,6 @@ def miehe_goektepe_lulei(C, mu, N, U, p, q):
     kwargs_stretch = {"mu": mu, "N": N}
     kwargs_tube = {"mu": mu * N * U}
 
-    scheme = BazantOh(n=21)
-
     return nonaffine_stretch(
-        C, p=p, f=langevin, kwargs=kwargs_stretch, quadrature=scheme
-    ) + nonaffine_tube(C, q=q, f=linear, kwargs=kwargs_tube, quadrature=scheme)
+        C, p=p, f=langevin, kwargs=kwargs_stretch
+    ) + nonaffine_tube(C, q=q, f=linear, kwargs=kwargs_tube)

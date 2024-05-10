8ff5b363daddba1bce77224b60ce731956c1d604
Andreas Dutzler <a.dutzler@gmail.com>
2024-05-10 22:22:57 +0200

    Update __init__.py

--- src/felupe/constitution/hyperelasticity/models/microsphere/__init__.py
+++ src/felupe/constitution/hyperelasticity/models/microsphere/__init__.py
@@ -1,14 +1,12 @@
-from ._chain import gauss, langevin, langevin2, linear
+from ._chain import langevin, linear
 from ._framework_affine import affine_stretch, affine_tube
 from ._framework_nonaffine import nonaffine_stretch, nonaffine_tube
 
 __all__ = [
     "affine_stretch",
     "affine_tube",
-    "gauss",
     "linear",
     "langevin",
-    "langevin2",
     "nonaffine_stretch",
     "nonaffine_tube",
 ]

657a9ae484e813c386dc0b3f93853e18943a43f8
Andreas Dutzler <a.dutzler@gmail.com>
2024-05-11 23:49:22 +0200

    Update __init__.py

--- src/felupe/constitution/hyperelasticity/__init__.py
+++ src/felupe/constitution/hyperelasticity/__init__.py
@@ -1,3 +1,17 @@
+"""
+Hyperelastic material model formulations.
+
+This module contains automatic-differentiation (ad) and non-ad based hyperelastic
+material model formulations:
+
+a) :class:`~felupe.Hyperelastic` to be used with strain energy density functions for
+   material model formulations from :mod:`constitution.hyperelasticity.models` which are
+   implemented with automatic differentiation using
+   `tensortrax <https://github.com/adtzlr/tensortrax>`_ and
+
+b) manually-defined hyperelastic material model formulations (no automatic
+   differentation) :mod:`constitution.hyperelasticity.core`.
+"""
 from ._hyperelastic import Hyperelastic
 
 __all__ = [

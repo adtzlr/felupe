d711fc985ec15ff4bdf24dc41bd1161113e954f7
Andreas Dutzler <a.dutzler@gmail.com>
2024-05-14 09:54:38 +0200

    Update _morph.py

--- src/felupe/constitution/hyperelasticity/models/_morph.py
+++ src/felupe/constitution/hyperelasticity/models/_morph.py
@@ -30,7 +30,7 @@ from tensortrax.math.linalg import det, inv, eigvalsh, expm
 
 
 def morph(C, statevars, p):
-    """Strain energy function of the
+    r"""Strain energy function of the
     `MORPH <https://doi.org/10.1016/s0749-6419(02)00091-8>`_ model formulation [1]_.
 
     Parameters
@@ -41,6 +41,98 @@ def morph(C, statevars, p):
         Vector of stacked state variables (CTS, C, SA).
     p : list of float
         A list which contains the 8 material parameters.
+
+    Notes
+    -----
+    The MORPH material model is implemented as a second Piola-Kirchhoff stress-based
+    formulation with automatic differentiation. The Tresca invariant of the distortional
+    part of the right Cauchy-Green deformation tensor is used as internal state
+    variable, see Eq. :eq:`morph-state`.
+
+    ..  warning::
+        While the `MORPH <https://doi.org/10.1016/s0749-6419(02)00091-8>`_-material
+        formulation captures the Mullins effect and quasi-static hysteresis effects of
+        rubber mixtures very nicely, it has been observed to be unstable for medium- to
+        highly-distorted states of deformation.
+
+    ..  math::
+        :label: morph-state
+
+        \boldsymbol{C} &= \boldsymbol{F}^T \boldsymbol{F}
+
+        I_3 &= \det (\boldsymbol{C})
+
+        \hat{\boldsymbol{C}} &= I_3^{-1/3} \boldsymbol{C}
+
+        \hat{\lambda}^2_\alpha &= \text{eigvals}(\hat{\boldsymbol{C}})
+
+        \hat{C}_T &= \max \left( \hat{\lambda}^2_\alpha - \hat{\lambda}^2_\beta \right)
+
+        \hat{C}_T^S &= \max \left( \hat{C}_T, \hat{C}_{T,n}^S \right)
+
+    A sigmoid-function is used inside the deformation-dependent variables
+    :math:`\alpha`, :math:`\beta` and :math:`\gamma`, see Eq. :eq:`morph-sigmoid`.
+
+    ..  math::
+        :label: morph-sigmoid
+
+        f(x) &= \frac{1}{\sqrt{1 + x^2}}
+
+        \alpha &= p_1 + p_2 \ f(p_3\ C_T^S)
+
+        \beta &= p_4\ f(p_3\ C_T^S)
+
+        \gamma &= p_5\ C_T^S\ \left( 1 - f\left(\frac{C_T^S}{p_6}\right) \right)
+
+    The rate of deformation is described by the Lagrangian tensor and its Tresca-
+    invariant, see Eq. :eq:`morph-rate-of-deformation`.
+
+    ..  note::
+        It is important to evaluate the incremental right Cauchy-Green tensor by the
+        difference of the final and the previous state of deformation, not by its
+        variation with respect to the deformation gradient tensor.
+
+    ..  math::
+        :label: morph-rate-of-deformation
+
+        \hat{\boldsymbol{L}} &= \text{sym}\left(
+                \text{dev}(\boldsymbol{C}^{-1} \Delta\boldsymbol{C})
+            \right) \hat{\boldsymbol{C}}
+
+        \lambda_{\hat{\boldsymbol{L}}, \alpha} &= \text{eigvals}(\hat{\boldsymbol{L}})
+
+        \hat{L}_T &= \max \left(
+            \lambda_{\hat{\boldsymbol{L}}, \alpha}-\lambda_{\hat{\boldsymbol{L}}, \beta}
+        \right)
+
+        \Delta\boldsymbol{C} &= \boldsymbol{C} - \boldsymbol{C}_n
+
+    The additional stresses evolve between the limiting stresses, see Eq.
+    :eq:`morph-stresses`. The additional deviatoric-enforcement terms [1]_ are neglected
+    in this implementation.
+
+    ..  math::
+        :label: morph-stresses
+
+        \boldsymbol{S}_L &= \left(
+            \gamma \exp \left(p_7 \frac{\hat{\boldsymbol{L}}}{\hat{L}_T}
+                \frac{\hat{C}_T}{\hat{C}_T^S} \right) +
+                p8 \frac{\hat{\boldsymbol{L}}}{\hat{L}_T}
+        \right) \boldsymbol{C}^{-1}
+
+        \boldsymbol{S}_A &= \frac{
+            \boldsymbol{S}_{A,n} + \beta\ \hat{L}_T\ \boldsymbol{S}_L
+        }{1 + \beta\ \hat{L}_T}
+
+        \boldsymbol{S} &= 2 \alpha\ \text{dev}( \hat{\boldsymbol{C}} )
+            \boldsymbol{C}^{-1}+\text{dev}\left(\boldsymbol{S}_A\ \boldsymbol{C}\right)
+            \boldsymbol{C}^{-1}
+
+    ..  note::
+        Only the upper-triangle entries of the symmetric stress-tensor state
+        variables are stored in the solid body. Hence, it is necessary to extract such
+        variables with :func:`tm.special.from_triu_1d` and export them as
+        :func:`tm.special.triu_1d`.
 
     Examples
     --------
@@ -81,6 +173,11 @@ def morph(C, statevars, p):
        rubberlike materials and its numerical applications", International Journal
        of Plasticity, vol. 19, no. 7. Elsevier BV, pp. 1019–1036, Jul. 2003. doi:
        `10.1016/s0749-6419(02)00091-8 <https://doi.org/10.1016/s0749-6419(02)00091-8>`_.
+
+    See Also
+    --------
+    felupe.morph_representative_directions : Strain energy function of the MORPH model
+        formulation, implemented by the concept of representative directions.
     """
 
     # extract old state variables
@@ -197,6 +294,10 @@ def morph_representative_directions(C, statevars, p, ε=1e-8):
        Journal of the Mechanics and Physics of Solids, vol. 52, no. 11. Elsevier BV, pp.
        2617–2660, Nov. 2004. doi:
        `10.1016/j.jmps.2004.03.011 <https://doi.org/10.1016/j.jmps.2004.03.011>`_.
+
+    See Also
+    --------
+    felupe.morph : Strain energy function of the MORPH model formulation.
     """
 
     def morph_uniaxial(λ, statevars, p, ε=1e-8):

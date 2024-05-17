4bca546df156e485f0d76583dd0ec2fc49b7108e
Andreas Dutzler <a.dutzler@gmail.com>
2024-05-17 13:27:52 +0200

    Enhance tutorial 2

--- docs/tutorial/examples/extut02_job.py
+++ docs/tutorial/examples/extut02_job.py
@@ -11,10 +11,13 @@ Run a Job
 
 This tutorial once again covers the essential high-level parts of creating and solving
 problems with FElupe. This time, however, the external displacements are applied in a
-ramped manner. The prescribed displacements of a cube under non-homogenous uniaxial
-loading will be controlled within a step. The Ogden-Roxburgh pseudo-elastic Mullins
-softening model is combined with an isotropic hyperelastic Neo-Hookean material
-formulation, which is further applied on a (nearly) incompressible solid body for a
+ramped manner. The prescribed displacements of a cube under non-homogenous
+:func:`uniaxial loading <felupe.dof.uniaxial>` will be controlled within a
+:class:`step <felupe.Step>`. The
+:class:`Ogden-Roxburgh <felupe.OgdenRoxburgh>` pseudo-elastic Mullins softening model is
+combined with an isotropic hyperelastic :class:`Neo-Hookean <felupe.NeoHooke>` material
+formulation, which is further applied on a
+:class:`nearly incompressible solid body <felupe.SolidBodyNearlyIncompressible>` for a
 realistic analysis of rubber-like materials. Note that the bulk modulus is now an
 argument of the (nearly) incompressible solid body instead of the constitutive
 Neo-Hookean material definition.
@@ -31,18 +34,30 @@ umat = fem.OgdenRoxburgh(material=fem.NeoHooke(mu=1), r=3, m=1, beta=0)
 body = fem.SolidBodyNearlyIncompressible(umat=umat, field=field, bulk=5000)
 
 # %%
-# The ramped prescribed displacements for 20 substeps are created with ``linsteps``.
-# A **Step** is created with a list of items to be considered (here, one single solid
-# body) and a dict of ramped boundary conditions along with the prescribed values.
-move = fem.math.linsteps([0, 2], num=10)
+# The ramped prescribed displacements for 12 substeps are created with
+# :func:`~felupe.math.linsteps`. A :class:`~felupe.Step` is created with a list of items
+# to be considered (here, one single solid body) and a dict of ramped boundary
+# conditions along with the prescribed values.
+move = fem.math.linsteps([0, 2, 1.5], num=[8, 4])
 uniaxial = fem.Step(
     items=[body], ramp={boundaries["move"]: move}, boundaries=boundaries
 )
 
 # %%
-# This step is now added to a **Job**. The results are exported after each completed and
-# successful substep as a time-series XDMF-file.
-job = fem.Job(steps=[uniaxial])
+# This step is now added to a :class:`~felupe.Job`. The results are exported after each
+# completed and successful substep as a time-series XDMF-file. A
+# :class:`~felupe.CharacteristicCurve`-job logs the displacement and sum of reaction
+# forces on a given boundary condition.
+job = fem.CharacteristicCurve(steps=[uniaxial], boundary=boundaries["move"])
 job.evaluate(filename="result.xdmf", verbose=True)
 
 field.plot("Principal Values of Logarithmic Strain").show()
+
+# %%
+# The sum of the reaction force in direction :math:`x` on the boundary condition
+# ``"move"`` is plotted as a function of the displacement :math:`u` on the boundary
+# condition ``"move"`` .
+fig, ax = job.plot(
+    xlabel="Displacement $u$ in mm $\longrightarrow$",
+    ylabel="Normal Force $F$ in N $\longrightarrow$",
+)

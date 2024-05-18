1ba30a977fca63b3eb0e3a82d26c95d67e4afd8b
Andreas Dutzler <a.dutzler@gmail.com>
2024-05-18 20:46:18 +0200

    Update _mesh.py

--- src/felupe/mesh/_mesh.py
+++ src/felupe/mesh/_mesh.py
@@ -1528,15 +1528,15 @@ class Mesh(DiscreteGeometry):
            :include-source: True
 
            >>> import felupe as fem
+           >>> import pyvista as pv
            >>>
            >>> mesh = fem.Cube(n=6)
            >>> mesh_with_midpoints_volumes = fem.mesh.add_midpoints_volumes(
            ...     mesh, cell_type_new="hexahedron9"
            ... )
-           >>>
-           >>> plotter = mesh.plot(opacity=0.5)
+           >>> plotter = pv.Plotter()
            >>> actor = plotter.add_points(mesh_with_midpoints_volumes.points, color="black")
-           >>> plotter.show()
+           >>> mesh.plot(opacity=0.5, plotter=plotter).show()
 
         >>> mesh_with_midpoints_volumes
         <felupe Mesh object>

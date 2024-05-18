80e7fcfc648bb99ee33652bcc2d3ff6253c34a46
Andreas Dutzler <a.dutzler@gmail.com>
2024-05-18 22:25:39 +0200

    Update _linear_elastic_plastic_isotropic.py

--- src/felupe/constitution/small_strain/models/_linear_elastic_plastic_isotropic.py
+++ src/felupe/constitution/small_strain/models/_linear_elastic_plastic_isotropic.py
@@ -128,11 +128,13 @@ def linear_elastic_plastic_isotropic_hardening(dε, εn, σn, ζn, λ, μ, σy, 
         ...     material=fem.linear_elastic_plastic_isotropic_hardening,
         ...     λ=2.0,
         ...     μ=1.0,
-        ...     σy=1.0,
+        ...     σy=0.05,
         ...     K=0.1,
         ...     dim=3,
         ...     statevars=(1, (3, 3)),
         ... )
+        >>> ux = fem.math.linsteps([1, 1.05, 0.95, 1.05], num=[10, 20, 20])
+        >>> ax = umat.plot(ux=ux, bx=None, ps=None)
 
     ..  pyvista-plot::
         :include-source: False

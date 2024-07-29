a2a974cc2a9d87469fb1ddbe81a5c497343a8583
Andreas Dutzler <a.dutzler@gmail.com>
2024-07-30 00:56:51 +0200

    Update _interpolate.py

--- src/felupe/mesh/_interpolate.py
+++ src/felupe/mesh/_interpolate.py
@@ -63,8 +63,7 @@ def interpolate_line(mesh, xi, axis, Interpolator=None, **kwargs):
         :context:
         :force_static:
 
-        >>> mesh_new = interpolate_line(mesh, xi=np.linspace(0, 1, 101), axis=1)
-        >>>
+        >>> mesh_new = fem.mesh.interpolate_line(mesh, xi=np.linspace(0, 1), axis=1)
         >>> mesh_new.plot(style="points", color="black").show()
     """
 

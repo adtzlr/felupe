f0d76bfafcc6075ee84bff56fb401b4dce495500
Andreas Dutzler <a.dutzler@gmail.com>
2024-08-17 23:20:11 +0200

    Test if region arrays are C-contiguous

--- tests/test_region.py
+++ tests/test_region.py
@@ -35,6 +35,9 @@ def test_region():
     mesh = fem.Rectangle()
     r = fem.RegionQuad(mesh, uniform=True)
 
+    assert r.h.flags["C_CONTIGUOUS"]
+    assert r.dhdX.flags["C_CONTIGUOUS"]
+
     r.plot(off_screen=True)
 
     r = fem.RegionQuadBoundary(mesh)
@@ -47,6 +50,9 @@ def test_region():
 
     r = fem.RegionQuadraticQuadBoundary(mesh2, ensure_3d=True)
 
+    assert r.h.flags["C_CONTIGUOUS"]
+    assert r.dhdX.flags["C_CONTIGUOUS"]
+
     mesh3 = fem.mesh.convert(mesh, 2, True, True, False)
     r = fem.RegionQuad(mesh3)
     r = fem.RegionQuadraticQuad(mesh3)
@@ -54,6 +60,9 @@ def test_region():
     f = fem.FieldsMixed(r)
 
     r = fem.RegionBiQuadraticQuadBoundary(mesh3)
+
+    assert r.h.flags["C_CONTIGUOUS"]
+    assert r.dhdX.flags["C_CONTIGUOUS"]
 
     mesh.cell_type = "some_fancy_cell_type"
     with pytest.raises(NotImplementedError):
@@ -69,6 +78,7 @@ def test_region():
     r = fem.RegionHexahedronBoundary(mesh, only_surface=False, mask=[0, 3])
 
     m = r.mesh_faces()
+
     assert np.allclose(m.points, mesh.points)
     assert m.cell_type == "quad"
 
@@ -88,6 +98,9 @@ def test_region():
 
     r = fem.RegionTriQuadraticHexahedronBoundary(mesh3)
 
+    assert r.h.flags["C_CONTIGUOUS"]
+    assert r.dhdX.flags["C_CONTIGUOUS"]
+
     triangle = fem.Triangle()
     points = triangle.points
     cells = np.arange(3).reshape(1, -1)
@@ -98,6 +111,9 @@ def test_region():
     r = fem.RegionTriangle(mesh2)
     r = fem.RegionQuadraticTriangle(mesh2)
     f = fem.FieldsMixed(r)
+
+    assert r.h.flags["C_CONTIGUOUS"]
+    assert r.dhdX.flags["C_CONTIGUOUS"]
 
     tetra = fem.Tetra()
     points = tetra.points
@@ -123,6 +139,9 @@ def test_region():
     mesh = fem.Mesh(points, cells, "tetra-mini")
     r = fem.RegionTetraMINI(mesh)
     f = fem.FieldsMixed(r)
+
+    assert r.h.flags["C_CONTIGUOUS"]
+    assert r.dhdX.flags["C_CONTIGUOUS"]
 
     order = 5
     dim = 3

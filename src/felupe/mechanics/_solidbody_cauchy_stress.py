39a17e3e23be5252c535e4eec39207a26a4620ea
Andreas Dutzler <a.dutzler@gmail.com>
2024-08-31 23:33:08 +0200

    Update _solidbody_cauchy_stress.py

--- src/felupe/mechanics/_solidbody_cauchy_stress.py
+++ src/felupe/mechanics/_solidbody_cauchy_stress.py
@@ -54,28 +54,29 @@ class SolidBodyCauchyStress:
         >>> mesh = fem.Rectangle(n=6)
         >>> region = fem.RegionQuad(mesh)
         >>> field = fem.FieldContainer([fem.FieldAxisymmetric(region, dim=2)])
-        >>> boundaries = fem.dof.symmetry(field[0])
-        >>> umat = fem.NeoHooke(mu=1, bulk=2)
-        >>> solid = fem.SolidBody(umat, field)
         >>>
+        >>> boundaries = {"fixed": fem.Boundary(field[0], fx=0)}
+        >>> solid = fem.SolidBody(umat=fem.NeoHooke(mu=1, bulk=2), field=field)
+        >>>
+        >>> mask = np.logical_and(mesh.x == 1, mesh.y > 0.5)
         >>> region_stress = fem.RegionQuadBoundary(
         ...     mesh=mesh,
-        ...     only_surface=True,  # select only faces on the outline
-        ...     mask=mesh.points[:, 0] == 1,  # select a subset of faces on the surface
-        ...     ensure_3d=True,  # requires True for axisymmetric/plane strain, otherwise False
+        ...     mask=mask,  # select a subset of faces on the surface
+        ...     ensure_3d=True,  # True for axisymmetric/plane strain
         ... )
         >>> field_boundary = fem.FieldContainer([fem.FieldAxisymmetric(region_stress, dim=2)])
         >>> stress = fem.SolidBodyCauchyStress(field=field_boundary)
         >>>
-        >>> table = fem.math.linsteps([0, 1], num=5, axis=0, axes=9).reshape(-1, 3, 3)
-        >>> step = fem.Step(
-        ...     items=[solid, stress], ramp={stress: 1 * table}, boundaries=boundaries
-        ... )
+        >>> table = (
+        ...     fem.math.linsteps([0, 1], num=5, axis=1, axes=9)
+        ...     + fem.math.linsteps([0, 1], num=5, axis=3, axes=9)
+        ... ).reshape(-1, 3, 3)
         >>>
+        >>> step = fem.Step(
+        ...     items=[solid, stress], ramp={stress: 1.0 * table}, boundaries=boundaries
+        ... )
         >>> job = fem.Job(steps=[step]).evaluate()
-        >>> solid.plot(
-        ...     "Principal Values of Cauchy Stress", component=2, clim=[-1.01, -0.99]
-        ... ).show()
+        >>> solid.plot("Principal Values of Cauchy Stress").show()
     """
 
     def __init__(self, field, cauchy_stress=None):

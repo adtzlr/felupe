5d15359aafbd3c939cbbc534d0d89e867f2224f7
Andreas Dutzler <a.dutzler@gmail.com>
2026-03-15 09:44:23 +0100

    Update test_thermal.py

--- tests/test_thermal.py
+++ tests/test_thermal.py
@@ -92,5 +92,79 @@ def test_thermal():
     )
 
 
+def test_thermal_axi():
+    mesh = fem.Rectangle(n=3)
+    region = fem.RegionQuad(mesh)
+    temperature = fem.Field(region, dim=1)
+    field = fem.FieldContainer([temperature])
+
+    region_convection = fem.RegionQuadBoundary(mesh, mask=mesh.y == 1.0)
+    temperature_convection = fem.FieldAxisymmetric(region_convection, dim=1)
+    field_convection = fem.FieldContainer([temperature_convection])
+
+    region_flux = fem.RegionQuadBoundary(mesh, mask=mesh.y == 1.0)
+    temperature_flux = fem.FieldAxisymmetric(region_flux, dim=1)
+    field_flux = fem.FieldContainer([temperature_flux])
+
+    boundaries = fem.BoundaryDict(
+        left=fem.Boundary(temperature, fx=0),
+        right=fem.Boundary(temperature, fx=1),
+    )
+
+    solid = fem.thermal.SolidBodyThermal(
+        field=field,
+        mass_density=1.0,  # kg/m^3
+        specific_heat_capacity=1.0,  # J/(kg*K)
+        time_step=0.01,  # s
+        thermal_conductivity=1.0,  # W/(m*K)
+        lumped_capacity=False,
+    )
+
+    solid = fem.thermal.SolidBodyThermal(
+        field=field,
+        mass_density=1.0,  # kg/m^3
+        specific_heat_capacity=1.0,  # J/(kg*K)
+        time_step=0.01,  # s
+        thermal_conductivity=1.0,  # W/(m*K)
+    )
+
+    convection = fem.thermal.SolidBodyThermalConvection(
+        field=field_convection,
+        coefficient=1.0,  # W/(m^2*K)
+        temperature=10.0,  # K
+    )
+
+    heat_flux = fem.thermal.SolidBodyThermalHeatFlux(
+        field=field_flux,
+        heat_flux=1.0,  # W/m^2
+    )
+
+    solid.assemble.vector(field)
+    solid.assemble.matrix(field)
+    convection.assemble.vector(field)
+    convection.assemble.matrix(field)
+    heat_flux.assemble.vector(field)
+    heat_flux.assemble.matrix(field)
+
+    time = fem.thermal.TimeStep([solid])
+    table = fem.math.linsteps([0, 1], num=2)
+    ramp = {
+        boundaries["right"]: 10 * table,
+        time: 0.1 * table,
+        convection: 100 * table,
+        heat_flux: 10 * table,
+    }
+    step = fem.Step(
+        items=[time, solid, convection, heat_flux], ramp=ramp, boundaries=boundaries
+    )
+    job = fem.Job(steps=[step]).evaluate(
+        filename="result.xdmf",  # result file for Paraview
+        point_data={"Temperature": lambda field, substep: temperature.values},
+        point_data_default=False,
+        cell_data_default=False,
+    )
+
+
 if __name__ == "__main__":
     test_thermal()
+    test_thermal_axi()

4e4ab74c1a85df209175664f7cd9c6b0e13c6f1b
AGeissler <achim.geissler@acatalepsy.ch>
2026-03-22 14:25:43 +0100

    Rename SolidBodyThermalSurfaceConvection to SolidBodyThermalSurfaceHeatTransfer

--- src/felupe/thermal/__init__.py
+++ src/felupe/thermal/__init__.py
@@ -1,11 +1,11 @@
 from ._solidbody_thermal import SolidBodyThermal
-from ._solidbody_thermal_convection import SolidBodyThermalConvection
+from ._solidbody_thermal_surface_heat_transfer import SolidBodyThermalSurfaceHeatTransfer
 from ._solidbody_thermal_heat_flux import SolidBodyThermalHeatFlux
 from ._time_step import TimeStep
 
 __all__ = [
     "SolidBodyThermal",
-    "SolidBodyThermalConvection",
+    "SolidBodyThermalSurfaceHeatTransfer",
     "SolidBodyThermalHeatFlux",
     "TimeStep",
 ]

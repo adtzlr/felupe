909dde1453670649bee837d015717c8f473da294
Achim Geissler <achim.geissler@fhnw.ch>
2026-04-27 17:40:17 +0200

    Add class 'SolidBodySurfaceConvection'. WIP, code save.

---
+++ src/felupe/thermal/_solidbody_surface_convection.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+"""
+This file is part of FElupe.
+
+FElupe is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FElupe is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FElupe.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import numpy as np
+from scipy.constants import sigma
+from scipy.sparse import csr_matrix
+
+from ..assembly import IntegralForm
+from ..mechanics import Assemble, Results, UpdateItem
+
+
+class SolidBodySurfaceConvection:
+    r"""Convective heat transfer on the surface of a thermal solid body.
+
+    Parameters
+    ----------
+    field : felupe.FieldContainer
+        Field container with the temperature in °C as first field.
+    convection coefficient : float | callable
+        Convection coefficient :math:`h_c` in W/(m^2 K).
+    temperature : float
+        The surrounding temperature :math:`\theta_{sur}` in °C.
+
+    Notes
+    -----
+    This class represents a boundary condition for a thermal solid body, which
+    is used to model convectinve heat transfer between the boundary of a
+    solid material and the adjacent ambient air with temperature
+    :math:`\theta_\infty` in °C.
+
+    The the heat flux at the boundary is calculated according to Eq.
+    :eq:`convective-flux`.
+
+    Eq. :eq: `example-horizontal-plate` gives an example for the detailed
+    calculation of the heat transfer coefficient for a warm horizontal plate
+    with heat flux upward. 'T' denotes temperatures in K. `Ra`is the Rayleigh
+    number, `Pr`is the Prandtl number (air) and `Nu` is the Nusselt number.
+
+    .. math::
+       :label: convective-flux
+
+       q_c = h_c\,\left(\theta_s - \theta_\infty\right)
+
+    .. math::
+       :label: example-horizontal-plate
+
+       \alpha = \frac{\lambda_{air}}{\rho_{air}cp_{air}}
+
+       \T_m = 0.5\left(T_s + T_\infty\right)
+
+       Ra = \frac{g \frac{1}{T_m}\left|\theta_s - \theta_\infty\right|\,L^3}{\alpha\mu}
+
+       Nu = 
+
+       h_c = Nu\,\lambda_{air}\,L
+
+
+    Examples
+    --------
+    ..  pyvista-plot::
+
+        >>> import felupe as fem
+        >>> import numpy as np
+        >>>
+        >>> mesh = fem.Rectangle(n=11)
+        >>> region = fem.RegionQuad(mesh)
+        >>> temperature = fem.Field(region, dim=1, values=20.0)
+        >>> field = fem.FieldContainer([temperature])
+        >>>
+        >>> region_radiation = fem.RegionQuadBoundary(mesh, mask=mesh.x == 1.0)
+        >>> temperature_radiation = fem.Field(region_radiation, dim=1)
+        >>> field_radiation = fem.FieldContainer([temperature_radiation])
+        >>>
+        >>> boundaries = fem.BoundaryDict(
+        ...     left=fem.Boundary(temperature, fx=0, value=20.0),
+        ... )
+        >>>
+        >>> solid = fem.thermal.SolidBodyThermal(
+        ...     field=field,
+        ...     mass_density=1400.0,  # kg / m^3
+        ...     specific_heat_capacity=1000.0,  # J / (kg K)
+        ...     time_step=720.0,  # s
+        ...     thermal_conductivity=1.0,  # W / (m K)
+        ... )
+        >>> radiation = fem.thermal.SolidBodySurfaceConvection(
+        ...     field=field_radiation,
+        ...     emissivity=0.8,
+        ...     temperature=20.0,  # °C
+        ... )
+        >>> time = fem.thermal.TimeStep([solid])
+        >>> table = fem.math.linsteps([0, 1], num=15)
+        >>> air_temperature = fem.math.linsteps([20, 40], num=15)  # air temperature
+        >>> emissivity = fem.math.linsteps([0.6, 0.8], num=15)  # a value between 0 ... 1
+        >>> ramp = {
+        ...     time: 18000 * table,  # five hours
+        ...     radiation["temperature"]: air_temperature,
+        ...     radiation["emissivity"]: emissivity,
+        ... }
+        >>> step = fem.Step(
+        ...     items=[time, solid, radiation], ramp=ramp, boundaries=boundaries
+        ... )
+        >>> job = fem.Job(steps=[step]).evaluate()
+        >>>
+        >>> mesh.view(
+        ...     point_data={"Temperature in °C": temperature.values}
+        ... ).plot("Temperature in °C").show()
+
+    See Also
+    --------
+    felupe.thermal.TimeStep : A time step item.
+    felupe.thermal.SolidBodyThermal : A thermal solid body for heat conduction.
+
+    """
+
+    def __init__(self, field, emissivity, temperature):
+        self.field = field
+        self.time_step = None
+
+        self.results = Results()
+        self.results.temperature = temperature  # ambient temperature in °C
+        self.results.emissivity = emissivity
+
+        self._sigma = sigma  # Stefan-Boltzmann constant
+
+        self.assemble = Assemble(
+            vector=self._vector, matrix=self._matrix, multiplier=-1.0
+        )
+
+    def __getitem__(self, key):
+        return UpdateItem(self, key)
+
+    def update(self, temperature):
+        self._update_temperature(temperature)
+
+    def _update_temperature(self, temperature):
+        self.results.temperature = temperature
+
+    def _update_emissivity(self, emissivity):
+        self.results.emissivity = emissivity
+
+    def _vector(self, field=None, **kwargs):
+        if field is not None:
+            self.field = field
+
+        if self.time_step is not None and self.time_step == 0:  # inactive time step
+            return csr_matrix(([0.0], ([0], [0])), shape=(1, 1))
+
+        temperature = self.field.extract(grad=False)[0]
+        fun = [
+            -self.results.emissivity
+            * self._sigma
+            * ((temperature + 273.15) ** 4 - (self.results.temperature + 273.15) ** 4)
+        ]
+
+        self.results.force = IntegralForm(
+            fun=fun, v=self.field, dV=self.field.region.dV, grad_v=[False]
+        ).assemble(**kwargs)
+
+        return self.results.force
+
+    def _matrix(self, field=None, **kwargs):
+        if field is not None:
+            self.field = field
+
+        if self.time_step is not None and self.time_step == 0:  # inactive time step
+            return csr_matrix(([0.0], ([0], [0])), shape=(1, 1))
+
+        dim = self.field[0].dim
+        temperature = self.field.extract(grad=False)[0]
+        fun = [
+            -self.results.emissivity
+            * self._sigma
+            * 4
+            * (temperature + 273.15) ** 3
+            * np.eye(dim).reshape(dim, dim, 1, 1)
+        ]
+
+        self.results.stiffness = IntegralForm(
+            fun=fun,
+            v=self.field,
+            u=self.field,
+            dV=self.field.region.dV,
+            grad_v=[False],
+            grad_u=[False],
+        ).assemble(**kwargs)
+
+        return self.results.stiffness

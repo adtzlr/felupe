f36c2f2fdcb439b49135d5aabed59c21e346d68f
Andreas Dutzler <a.dutzler@gmail.com>
2026-07-03 11:37:37 +0200

    re-add examples

---
+++ examples/ex12_foot-bone.py
@@ -0,0 +1,49 @@
+r"""
+Voxelized Foot Bones
+--------------------
+A :class:`~felupe.Region` on a voxel-based mesh with uniform hexahedrons should be
+created with ``uniform=True`` to enhance performance.
+
+.. admonition:: This example requires external packages.
+   :class: hint
+
+   .. code-block::
+
+      pip install pypardiso
+"""
+
+import numpy as np
+import pypardiso
+
+import felupe as fem
+
+mesh = fem.mesh.read("ex12_foot-bones_mesh-voxels.vtu")[0]
+
+region = fem.RegionHexahedron(mesh, uniform=True)
+field = fem.FieldContainer([fem.Field(region, dim=3)])
+boundaries = {
+    "fixed": fem.Boundary(field[0], fx=lambda x: x <= -110),
+    "fixed_plane": fem.Boundary(field[0], fz=mesh.z.min()),
+}
+
+umat = fem.LinearElastic(E=1000, nu=0.3)
+solid = fem.SolidBody(umat, field)
+gravity = fem.SolidBodyForce(field, values=[0, 0, -7e-2])
+bottom = fem.ContactRigidPlane(
+    field,
+    points=np.arange(mesh.npoints),
+    centerpoint=-1,
+    normal=[0, 0, 1],
+    friction=0.0,
+    items=[solid],
+)
+
+step = fem.Step(items=[solid, gravity, bottom], boundaries=boundaries)
+job = fem.Job(steps=[step]).evaluate(solver=pypardiso.spsolve, parallel=True)
+plotter = solid.plot(
+    "Principal Values of Cauchy Stress",
+    show_edges=False,
+    show_undeformed=False,
+    clim=[0, 10],
+)
+bottom.plot(plotter=plotter, color="lightgrey", opacity=1.0).show()
